99980e1f43b9fe90718b5b6578ada001258b13fb
mengxiangtai <wei5899584@gmail.com>
2022-04-01 15:30:42 +0900

    first push

---
+++ text.js
@@ -0,0 +1,9 @@
+let val = 3;
+console.log(val == 3);
+
+let age = 21;
+let result = (age >= 20)? "daren" : "xiaoren"
+console.log(result);
+
+let array = ['item1','item2','item3']
+console.log(array);
